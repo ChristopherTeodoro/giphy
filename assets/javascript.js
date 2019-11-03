a15516976f3bfc0491cec678c4f83368d7718b6f
Christopher Teodoro <clt2me@yahoo.com>
2019-11-02 22:13:16 -0500

    adding code to fall back on

---
+++ assets/javascript.js
@@ -0,0 +1,9 @@
+$(function(){
+    console.log("page loaded");
+})
+
+var topics=["Uranus","Mars","Venus"];
+
+function populateButtons(topics,classToAdd,areaToAddTo){
+    $(areaToAddTo).empty()
+}
