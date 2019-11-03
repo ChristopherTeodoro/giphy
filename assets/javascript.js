e4811df50582e9219954b5870f3816c92ce739f3
Christopher Teodoro <clt2me@yahoo.com>
2019-11-02 23:29:22 -0500

    late night commit

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,9 +1,22 @@
 $(function(){
+    populateButtons(topics,'searchButton','#buttons-Div');
     console.log("page loaded");
 })
 
-var topics=["Uranus","Mars","Venus"];
+var topics = ['Uranus','Mars','Venus'];
 
 function populateButtons(topics,classToAdd,areaToAddTo){
-    $(areaToAddTo).empty()
-}
+    $(areaToAddTo).empty();
+    for(var i=0;i<topics.length;i++){
+        var a = $('<button>');
+        a.addClass(classToAdd);
+        a.attr('data-type', topics[i]);
+        a.text(topics[i]);
+        $(areaToAddTo).append(a);
+    }
+   }
+
+   $(document).on('click','.searchButton',function(){
+        var type = $(this).data('type');
+        console.log('type');
+   })
